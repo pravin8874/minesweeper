fa50099ec4fb4ae23a1fcce120352227ee163a67
Pravin <ppal@isystango.com>
2019-07-15 11:31:47 +0530

    Initial commit.

---
+++ README.md
@@ -0,0 +1,51 @@
+#MINESWEEPER
+
+- Install all dependency
+  `yarn install` or `npm install`
+- Start app
+  `yarn start` or `npm start`
+- Inseart number of rows and columns through command prompt.
+  ```html
+  Please enter the no of rows 3
+  Please enter the no of coloumns 6
+  ```
+- Inseart data into matrix.
+  ```html
+  ================================
+  Insert element of matrix a[0][0] 1
+  Insert element of matrix a[0][1] 0
+  Insert element of matrix a[0][2] 1
+  Insert element of matrix a[0][3] 1
+  Insert element of matrix a[0][4] 0
+  Insert element of matrix a[0][5] 1
+  Insert element of matrix a[1][0] 0
+  Insert element of matrix a[1][1] 0
+  Insert element of matrix a[1][2] 1
+  Insert element of matrix a[1][3] 1
+  Insert element of matrix a[1][4] 1
+  Insert element of matrix a[1][5] 0
+  Insert element of matrix a[2][0] 1
+  Insert element of matrix a[2][1] 0
+  Insert element of matrix a[2][2] 0
+  Insert element of matrix a[2][3] 1
+  Insert element of matrix a[2][4] 1
+  Insert element of matrix a[2][5] 0
+  ================================
+  ```
+  - Show output like this
+  ```html
+  ================================
+  0  1  2  3  4  5
+  -  -  -  -  -  -
+  1  0  1  1  0  1
+  0  0  1  1  1  0
+  1  0  0  1  1  0
+
+  ================================
+  0  1  2  3  4  5
+  -  -  -  -  -  -
+  9  3  9  9  4  9
+  2  4  9  9  9  3
+  9  2  3  9  9  2
+  ================================
+  ```
---
+++ minesweeper.js
@@ -0,0 +1,49 @@
+const ask = require("p-readline")
+const cTable = require('console.table');
+
+playGame = async () => {
+    const row = await ask("Please enter the no of rows");
+    const col = await ask("Please enter the no of coloumns");
+    const matrix = [];
+    const finalMatrix = [];
+    console.log("================================")
+    console.error("Note: greater than 0 is consider as a Mine and space or 0 is safe place.")
+    console.log("================================")
+    for (i = 0; i < row; i++) {
+      matrix[i] = [];
+      for (j = 0; j < col; j++) {
+        const ele = await ask(`Insert element of matrix a[${i}][${j}]`, {defaultValue: 0})
+        matrix[i][j] = ele;
+      }
+    }
+
+    for (i = 0; i < row; i++) {
+      finalMatrix[i] = [];
+      for (j = 0; j < col; j++) {
+        if (+matrix[i][j]) {
+          finalMatrix[i][j] = 9;
+        } else {
+          let count = 0;
+          for(k = i - 1; k < i + 2; k++) {
+            for(l = j - 1; l < j + 2; l++) {
+              const selfEle = (i == k && j == l);
+              if (((-1 < k) && (k < row) && (-1 < l) && (l < col)) && !selfEle) {
+                if (+matrix[k][l])
+                  ++count
+              }
+            }
+          }
+          finalMatrix[i][j] = count;
+        }
+      }
+    }
+
+    console.log("================================")
+    console.table(matrix)
+    console.log("================================")
+    console.table(finalMatrix)
+}
+ 
+playGame()
+
+
