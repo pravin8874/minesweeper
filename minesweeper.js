32dea05faa8484c72b3a673258dac9e7418b22b4
Pravin <ppal@isystango.com>
2019-07-17 19:34:10 +0530

    Update variable names.

--- minesweeper.js
+++ minesweeper.js
@@ -3,25 +3,25 @@ const cTable = require('console.table');
 
 playGame = async () => {
   // Here we taking rows and coloumns of board matrix.
-  const row = await ask("Please enter the no of rows");
-  const col = await ask("Please enter the no of coloumns");
+  const noOfRows = await ask("Please enter the no of rows");
+  const noOfColumns = await ask("Please enter the no of columns");
   const inputMatrix = [];
   const finalMatrix = [];
   console.log("================================")
   console.error("Note: greater than 0 is consider as a Mine and space or 0 is empty place.")
   console.log("================================")
   // Here we taking inputs of board matrix.
-  for (let rowIndex = 0; rowIndex < row; rowIndex++) {
+  for (let rowIndex = 0; rowIndex < noOfRows; rowIndex++) {
     inputMatrix[rowIndex] = [];
-    for (let colIndex = 0; colIndex < col; colIndex++) {
+    for (let colIndex = 0; colIndex < noOfColumns; colIndex++) {
       const ele = await ask(`Insert element of matrix a[${rowIndex}][${colIndex}]`, { defaultValue: 0 })
       inputMatrix[rowIndex][colIndex] = ele;
     }
   }
 
-  for (let rowIndex = 0; rowIndex < row; rowIndex++) {
+  for (let rowIndex = 0; rowIndex < noOfRows; rowIndex++) {
     finalMatrix[rowIndex] = [];
-    for (colIndex = 0; colIndex < col; colIndex++) {
+    for (colIndex = 0; colIndex < noOfColumns; colIndex++) {
       // If element is greater than 0 then set 9 as mine
       if (+inputMatrix[rowIndex][colIndex]) {
         finalMatrix[rowIndex][colIndex] = 9;
@@ -30,9 +30,9 @@ playGame = async () => {
         // If element is 0 or space than set no of adjacent mines
         for (let adjRowIndex = rowIndex - 1; adjRowIndex < rowIndex + 2; adjRowIndex++) {
           for (let adjColIndex = colIndex - 1; adjColIndex < colIndex + 2; adjColIndex++) {
-            // Here we find self elements.
+            // Here we find self element.
             const selfEle = (rowIndex == adjRowIndex && colIndex == adjColIndex);
-            if (((-1 < adjRowIndex) && (adjRowIndex < row) && (-1 < adjColIndex) && (adjColIndex < col)) && !selfEle) {
+            if (((-1 < adjRowIndex) && (adjRowIndex < noOfRows) && (-1 < adjColIndex) && (adjColIndex < noOfColumns)) && !selfEle) {
               if (+inputMatrix[adjRowIndex][adjColIndex])
                 ++noOfAdjacent
             }
